3164e7e34b4a97dafcfcf88c0ce5b62a554adb4e
steven.t.homer@gmail.com <steven.t.homer@gmail.com>
2019-11-15 13:23:40 +0100

    Documentation: Config

--- src/config.rs
+++ src/config.rs
@@ -1,35 +1,41 @@
-//use std::fs;
-
+/// Configuration for loading/saving, as well as parameter dimensions
 pub struct Config {
+    /// Path of WAV file to load audio from
     pub load_from: String,
-    //fs::File,
+    /// Path of file to save dimensions to
     pub save_at: String,
-    //fs::File,
+    /// Path of file to load dimensions from
     pub init_with: String,
-    //fs::File,
+    /// Scale of the initial radius of a category
     pub radius_scale: f64,
+    /// Number of real + virtual concepts in a trajectory
     pub resolution: u16,
+    /// Maximum number of dimensions in the memory.  Since each additional level
+    /// is exponentially more expensive, this can be used to limit computation.
     pub max_depth: u16,
 }
 
 impl Config {
+    /// Returns a Config without paths, and default values for dimension params.
     pub fn default() -> Result<Config, &'static str> {
         Ok(Config {
-            load_from: "".to_string(), //fs::File(),
-            save_at: "".to_string(),   //fs::File(),
-            init_with: "".to_string(), //fs::File(),
+            load_from: "".to_string(),
+            save_at: "".to_string(),
+            init_with: "".to_string(),
             radius_scale: 1.0,
             resolution: 256,
             max_depth: 4,
         })
     }
 
+    /// Returns a Config parameterized by the given command-line arguments.
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        unimplemented!();
         // TODO: Parse arguments
         Ok(Config {
-            load_from: "".to_string(), //fs::File(),
-            save_at: "".to_string(),   //fs::File(),
-            init_with: "".to_string(), //fs::File(),
+            load_from: "".to_string(),
+            save_at: "".to_string(),
+            init_with: "".to_string(),
             radius_scale: 1.0,
             resolution: 256,
             max_depth: 4,
